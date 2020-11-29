1be5c9715e8201497688cfc8208d551d3e27572e
yuichirosaito <l.0821.enjoy@icloud.com>
2020-11-29 13:38:48 +0900

    ユーザー管理機能（コードレビュー③）

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,12 +3,12 @@ FactoryBot.define do
     nickname {Faker::Name.last_name}
     email {Faker::Internet.free_email}
     password = Faker::Internet.password(min_length: 6)
-    password {password+"a"+"1"}
+    password {password}
     password_confirmation {password}
-    last_name {Faker::Name.last_name}   
-    first_name {Faker::Name.first_name}      
-    last_name_kana {Faker::Name.last_name}   
-    first_name_kana {Faker::Name.last_name}  
+    last_name {"田中"}   
+    first_name {"田中"}      
+    last_name_kana {"タナカ"}   
+    first_name_kana {"タナカ"}  
     birthday {Faker::Date.between(from: '2014-09-23', to: '2020-09-25')} 
   end
 end
