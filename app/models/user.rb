34b6de2a96004636813c14369d163217916a6be9
yuichirosaito <l.0821.enjoy@icloud.com>
2020-11-29 16:09:48 +0900

    ユーザー管理機能（コードレビュー⑥）

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,13 @@ class User < ApplicationRecord
     validates :email, format: { with:/\A([^@\s]+)@((?:[-a-z0-9]+.)+[a-z]{2,})\z/i}
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i}
     validates :birthday
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+    with_options format: format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."} do
+     validates :first_name_kana
+     validates :last_name_kana
+      with_options format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."} do
+       validates :first_name
+       validates :last_name
+      end 
+    end
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,9 +2,8 @@ FactoryBot.define do
   factory :user do
     nickname {Faker::Name.last_name}
     email {Faker::Internet.free_email}
-    password = Faker::Internet.password(min_length: 6)
-    password {password+"1a"}
-    password_confirmation {password+"1a"}
+    password { Faker::Internet.password(min_length: 6)+"1a"}
+    password_confirmation{password}
     last_name {"田中"}   
     first_name {"田中"}      
     last_name_kana {"タナカ"}   
