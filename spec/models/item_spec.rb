2a22ae377d38c82584c7b88b5145d7280b9f3a1a
yuichirosaito <l.0821.enjoy@icloud.com>
2020-11-30 23:01:26 +0900

    商品一覧表示機能（コードレビュー①）

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -67,7 +67,7 @@ RSpec.describe Item, type: :model do
       end
 
       it '価格の範囲が、¥300~¥9,999,999の間であること' do
-        @item.price = 10000000
+        @item.price = 10_000_000
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
