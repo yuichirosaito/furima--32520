4890a507bc8f735afe088ae2045eda8e3d9a588f
yuichirosaito <l.0821.enjoy@icloud.com>
2020-11-30 20:14:17 +0900

    （コードレビュー④）

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -66,6 +66,12 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
 
+      it '価格の範囲が、¥300~¥9,999,999の間であること' do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
+      end
+
       it '販売価格は半角数字のみ保存可能であること' do
         @item.price = '@@@'
         @item.valid?
