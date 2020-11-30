ec879ef2e31643b6b1f1b6c7f47fa209d9be40e3
yuichirosaito <l.0821.enjoy@icloud.com>
2020-11-30 17:48:12 +0900

    商品出品機能（コードレビュー①）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,20 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :explaination, :category_id, :status_id, :price, :delivery_fee_id, :image, :delivery_area_id, :delivery_span_id).merge(user_id: current_user.id)
   end
 end
