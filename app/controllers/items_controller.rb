d00de94b5a2901220a888feb7897321e23fa5beb
yuichirosaito <l.0821.enjoy@icloud.com>
2020-12-02 12:51:15 +0900

    商品削除機能（コードレビュー④）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,7 +38,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if current_user.id != @item.user_id 
+    if current_user.id == @item.user_id 
       if @item.destroy
       redirect_to root_path
       end
