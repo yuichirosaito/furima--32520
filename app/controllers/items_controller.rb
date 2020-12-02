77d39d3109e9f944494337489e544fc45cfc2058
yuichirosaito <l.0821.enjoy@icloud.com>
2020-12-02 10:07:06 +0900

    商品情報編集機能（コードレビュー②）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :update]
+
+
   def index
     @items = Item.includes(:user).order("created_at DESC")
   end
@@ -21,14 +24,12 @@ class ItemsController < ApplicationController
   end  
 
   def edit
-    @item = Item.find(params[:id])
     if user_signed_in? && current_user.id != @item.user_id
       redirect_to root_path
     end  
   end
   
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to root_path
     else
@@ -40,5 +41,9 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:name, :explaination, :category_id, :status_id, :price, :delivery_fee_id, :image, :delivery_area_id, :delivery_span_id).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
